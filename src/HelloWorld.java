c1bdf6cbc984830c045a2e11a15cde4112f58210
elizabeth.mulenga@yandex.com <elizabeth.mulenga@yandex.com>
2022-12-26 15:07:57 +0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+
+public class HelloWorld {
+    public static void main(String[] args){
+        String firstName = "Elly";
+        String secondName = "Mulenga";
+        System.out.println("Elly "+ "Mulenga" );
+    }
+}
